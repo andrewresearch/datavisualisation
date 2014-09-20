df1177b808624d9d638bfd748c09e710d6b84020
andrew <andrew.gibson@qut.edu.au>
2014-09-20 12:53:37 +1000

    Initial starter code

---
+++ starterCode/js/starter.js
@@ -0,0 +1,7 @@
+// Our local javascript file - this is where we write our code
+
+var alertFunction = function (message) {
+    alert("The message is: "+message);
+}
+
+//alertFunction("hello"); //Uncomment the beginning of this line to execute the alertFunction
